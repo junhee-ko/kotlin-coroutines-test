9f6b17dfd1248bed3703408a9f24fa9b531f209e
junhee-ko <junheee.ko@gmail.com>
2026-01-04 19:42:50 +0900

    use TestCoroutineScheduler

--- src/test/kotlin/NumberServiceTest.kt
+++ src/test/kotlin/NumberServiceTest.kt
@@ -1,4 +1,7 @@
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineScheduler
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -14,6 +17,26 @@ class NumberServiceTest{
         service.fetchNumberAsync()
 
         Thread.sleep(6_000)
+
+        // then
+        assertEquals(1, service.fetchedNumber)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun use_scheduler() {
+        // given
+        val scheduler = TestCoroutineScheduler()
+        val dispatcher = StandardTestDispatcher(scheduler)
+
+        val service = NumberService(dispatcher)
+
+        // when
+        service.fetchNumberAsync()
+
+        scheduler.advanceTimeBy(5000)
+        scheduler.runCurrent()
+//        scheduler.advanceUntilIdle()
 
         // then
         assertEquals(1, service.fetchedNumber)
